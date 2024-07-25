71e02d8b60c47a7e914291681f6f49ab2a06a630
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-25 12:32:34 +0500

    Created Reducer

--- src/features/todo/todoSlice.jsx
+++ src/features/todo/todoSlice.jsx
@@ -1,1 +1,50 @@
+//nano id is used to generate unique id,s
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello World" }],
+};
+
+function sayhello() {
+  console.log("hello");
+}
+
+//reducers contain properties and functions
+
+reducers: {
+    // addTodo: sayhello,
+
+    addTodo: (state, action) => {
+      //state is used to check initial state
+      //action is used to perform opr i.d to give id to remove an element
+
+      const todo = {
+        //id: 1,
+        id: nanoid(),
+        //text: "Hello World",
+        //also
+        //  text:action.payload.text
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => {
+        todo.id !== action.payload;
+      });
+    },
+    //updated reducer
+  },
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+reducers:{...}
+  
+
+});
+
+export const {addTodo,removeTodo}=todoSlice.actions
+
+export default todoSlice.reducer
