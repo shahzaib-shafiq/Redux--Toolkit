08d3f4b69f587969b751f282a232313787347bdf
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-25 15:24:24 +0500

    Modified Slice Code

--- src/features/todo/todoSlice.jsx
+++ src/features/todo/todoSlice.jsx
@@ -1,50 +1,36 @@
-//nano id is used to generate unique id,s
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+////nano id is used to generate unique id,s
 
 const initialState = {
   todos: [{ id: 1, text: "Hello World" }],
 };
 
+// Function is not necessary to define here unless used elsewhere
 function sayhello() {
   console.log("hello");
 }
 
-//reducers contain properties and functions
-
-reducers: {
-    // addTodo: sayhello,
-
-    addTodo: (state, action) => {
-      //state is used to check initial state
-      //action is used to perform opr i.d to give id to remove an element
-
-      const todo = {
-        //id: 1,
-        id: nanoid(),
-        //text: "Hello World",
-        //also
-        //  text:action.payload.text
-        text: action.payload,
-      };
-      state.todos.push(todo);
-    },
-
-    removeTodo: (state, action) => {
-      state.todos = state.todos.filter((todo) => {
-        todo.id !== action.payload;
-      });
-    },
-    //updated reducer
-  },
-
-export const todoSlice = createSlice({
+const todoSlice = createSlice({
   name: "todo",
   initialState,
-reducers:{...}
-  
-
+  reducers: {
+    addTodo: (state, action) => {
+      // Creating a new todo item
+      const todo = {
+        id: nanoid(),
+        text: action.payload, // Using payload directly for text
+      };
+      // Adding new todo to the state
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      // Filtering out the todo with the matching id
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
 });
 
-export const {addTodo,removeTodo}=todoSlice.actions
+export const { addTodo, removeTodo } = todoSlice.actions;
 
-export default todoSlice.reducer
+export default todoSlice.reducer;
